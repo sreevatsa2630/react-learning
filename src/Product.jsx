5e01cd33ee511361dd79f819e517663534e9e1a4
sreevatsa2630 <sreevatsakulkarni26@gmail.com>
2025-04-15 19:13:22 +0530

    7:13

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,11 @@
 import "./Product.css";
 
-function Product({title, price}){
+function Product({title, price,features2}){
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price: {price}</h5>
+            <h5>{features2.a}</h5>
         </div>
     );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,9 +1,11 @@
 import Product from "./Product"
 
 function ProductTab(){
+    let options = ["hi-tech","durable","fast"];
+    let options2 = {a: "hi-tech", b: "durable",c: "fast"};
     return (
         <>
-        <Product title="Phone" price= {30000}/>
+        <Product title="Phone" price= {30000} features={options} features2={options2}/>
         <Product title="Laptop" price= {40000}/>
         <Product title="Pen" price= {10000}/>
         </>
