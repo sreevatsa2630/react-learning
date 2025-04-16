a6d34f1876cb2a0f0bbb5b05d4013562063910f8
sreevatsa2630 <sreevatsakulkarni26@gmail.com>
2025-04-16 15:32:32 +0530

    3:32

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css"
+import Button from "./Button";
 // import
 import ProductTab from "./ProductTab";
-
+import Form from "./Form";
 // creating a component
 function App() {
   return (
     <>
-        <h2>Blockbuster Deals | Shop Now</h2>
-    <ProductTab/>
+    <Form/>
+    {/* <Button/> */}
+        {/* <h2>Blockbuster Deals | Shop Now</h2>
+    <ProductTab/> */}
     </>
   );
 }
